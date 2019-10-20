2b693b21339f3266bb376b24184cce74cafd7b3b
Andre Rocha <andrefmrocha@live.com.pt>
2019-10-20 15:43:50 +0100

    Refactored into different files

---
+++ board_pieces.pl
@@ -0,0 +1,21 @@
+clear :- write('\e[2J').
+
+ulc :- ansi_format([fg(cyan)], '~c', [9556]). % ╔ : upper left corner
+urc :- ansi_format([fg(cyan)], '~c', [9559]). % ╗ : upper right corner
+llc :- ansi_format([fg(cyan)], '~c', [9562]). % ╚ : lower left corner
+lrc :- ansi_format([fg(cyan)], '~c', [9565]). % ╝ : lower right corner
+
+hdiv :- ansi_format([fg(cyan)], '~c', [9552]), ansi_format([fg(cyan)], '~c', [9552]). % ═ : horizontal division
+vdiv :- ansi_format([fg(cyan)], '~c', [9553]). % ║ : vertical division
+mdiv :- ansi_format([fg(cyan)], '~c', [9580]). % ╬ : middle division
+
+tr :- ansi_format([fg(cyan)], '~c', [9568]). % ╠ : T right
+tl :- ansi_format([fg(cyan)], '~c', [9571]). % ╣ : T left
+td :- ansi_format([fg(cyan)], '~c', [9574]). % ╦ : T down
+tu :- ansi_format([fg(cyan)], '~c', [9577]). % ╩ : T up
+
+wt:- ansi_format([bold, fg(white)], '~c', [11044]), write(' '). % White piece
+bl:- ansi_format([bold, fg(black)], '~c', [11044]), write(' '). % Black piece
+null:- write('  '). % Null space
+corner:- write(' '). % Board corner
+empty:- ansi_format([bold, bg(cyan)], '~s', [' ']), ansi_format([bold, bg(cyan)], '~s', [' ']). % Empty space
---
+++ display.pl
@@ -0,0 +1,68 @@
+:- use_module(library(ansi_term)).
+:- ensure_loaded('board_pieces.pl').
+:- ensure_loaded('board_states.pl').
+
+middle_separator :- write(' '), tr, hdiv, mdiv, hdiv, mdiv, hdiv, mdiv, hdiv, mdiv, hdiv, mdiv, hdiv, mdiv, hdiv, mdiv, hdiv, tl, nl.
+
+second_separator :- write(' '), ulc, hdiv, mdiv, hdiv, mdiv, hdiv, mdiv, hdiv, mdiv, hdiv, mdiv, hdiv, mdiv, hdiv, mdiv, hdiv, urc, nl.
+
+penultimate_separator :- write(' '), llc, hdiv, mdiv, hdiv, mdiv, hdiv, mdiv, hdiv, mdiv, hdiv, mdiv, hdiv, mdiv, hdiv, mdiv, hdiv, lrc, nl.
+
+first_separator :- write(' '), ulc, hdiv, td, hdiv, td, hdiv, td, hdiv, td, hdiv, td, hdiv, urc, nl.
+
+last_separator :- write(' '), write('   '), llc, hdiv, tu, hdiv, tu, hdiv, tu, hdiv, tu, hdiv, tu, hdiv, lrc, nl.
+
+% general_line_display(Line, Row_Number). -> 2 - 6
+general_line_display([],_).
+general_line_display([H | []], N):-
+	display_last_line(H, N).
+general_line_display([H | T], N) :-
+	middle_separator,
+	write(N), vdiv, display_line(H), nl,
+	N1 is N + 1,
+	general_line_display(T, N1).
+
+% display_second_line(Line, Row_Number). -> 1
+display_second_line([H | T], N) :-
+	second_separator,
+	write(N), vdiv, display_line(H), nl,
+	N1 is N + 1,
+	general_line_display(T, N1).
+
+% display_first_line(Line, Row_Number). -> 0
+display_first_line([H | T], N) :-
+	write('   '), first_separator,
+	write(N), write('  '), display_line(H), nl,
+	N1 is N + 1,
+	display_second_line(T, N1).
+
+% display_last_line(Line, Row_Number). -> 7
+display_last_line(Line, N) :-
+	penultimate_separator,
+    write(N),
+	write('  '), display_line(Line), nl,
+	last_separator.
+
+% display_line(Line).
+display_line([]).
+display_line([corner | []]):-
+	corner,
+	null.
+display_line([Element | Rest]) :- 
+	Element, vdiv,
+	display_line(Rest).
+
+display_game(Board, Player):-
+	write('Player turn: '),
+	write(Player), nl, nl,
+	write('   A  B  C  D  E  F  G  H'),nl,
+	display_first_line(Board, 0).
+
+cute_display :- 
+	clear,
+	final_board(X),
+	display_game(X, 'John Doe').
+
+
+
+
