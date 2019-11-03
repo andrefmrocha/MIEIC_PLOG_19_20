ab7f5b34f7765bfb7c7a2befe6752ea5b4ac0fb2
ManelCoutinho <manuel.monge.coutinho@gmail.com>
2019-11-03 13:25:44 +0000

    Quick refactor

--- src/display.pl
+++ src/display.pl
@@ -3,74 +3,54 @@
 :- ensure_loaded('board_generation.pl').
 
 % created to ensure there's a clear separation line between board lines
-middle_separator(Line) :- 
-	write(' '), tr, 
-	middle_separator_helper(Line, 'M').
-
-% creates a separator between the board and the upper left and right corners
-second_separator(Line) :- 
-	write(' '), ulc, 
-	middle_separator_helper(Line, 'S').
+middle_separator(Line, Head, Tail) :- 
+	write(' '), Head, 
+	middle_separator_helper(Line, Tail).
 
 % create a separator between the board and the bottom left and right corners
-middle_separator_helper([_ | []], 'S') :- hdiv, urc, nl.
-middle_separator_helper([_ | []], 'M') :- hdiv, tl, nl.
-middle_separator_helper([_ | []], 'P') :- hdiv, lrc, nl.
+middle_separator_helper([_ | []], Element) :- hdiv, Element, nl.
 
 middle_separator_helper([_ | T], Last) :- hdiv, mdiv, middle_separator_helper(T, Last).
 
-penultimate_separator(Line) :- 
-	write(' '), llc,
-	middle_separator_helper(Line, 'P').
+% creates the outskirt lines for board
+outskirt_separator_helper([_ | []], _, End) :- hdiv, End, nl.
 
-% creates the line first line for board
-first_separator_helper([_ | []]) :- hdiv, urc, nl.
+outskirt_separator_helper([_ | R], Middle, End) :- hdiv, Middle, outskirt_separator_helper(R, Middle, End).
 
-first_separator_helper([_ | T]) :- hdiv, td, first_separator_helper(T).
-
-first_separator([_, _ | Tail]) :- 
-	write(' '), ulc,
-	first_separator_helper(Tail).  
-
-% creates the last line for the board
-last_separator_helper([_ | []]) :- hdiv, lrc, nl.
-
-last_separator_helper([_ | T]) :- hdiv, tu, last_separator_helper(T).
-
-last_separator([_, _ | Tail]) :- 
-	write(' '), llc, 
-	last_separator_helper(Tail).
+outskirt_separator([_, _ | Rest], Head, Middle, End) :-
+	write(' '), Head,
+	outskirt_separator_helper(Rest, Middle, End).
 
 % general_line_display(Line, Row_Number). -> 2 - 6
 general_line_display([],_).
 general_line_display([H | []], N):-
 	display_last_line(H, N), !.
 general_line_display([H | T], N) :-
-	middle_separator(H),
+	middle_separator(H, tr, tl),
 	write(N), vdiv, display_line(H), nl, !,
 	N1 is N + 1,
 	general_line_display(T, N1).
 
 % display_second_line(Line, Row_Number). -> 1
 display_second_line([H | T], N) :-
-	second_separator(H),
+	middle_separator(H, ulc, urc),
 	write(N), vdiv, display_line(H), nl, !,
 	N1 is N + 1,
 	general_line_display(T, N1).
 
 % display_first_line(Board, Row_Number). -> 0
 display_first_line([H | T], N) :-
-	write('   '), first_separator(H),
+	write('   '), outskirt_separator(H, ulc, td, urc),
 	write(N), write('  '), display_line(H), nl, !,
 	N1 is N + 1,
 	display_second_line(T, N1).
 
 % display_last_line(Line, Row_Number). -> 7
 display_last_line(Line, N) :-
-	penultimate_separator(Line),
+	middle_separator(Line, llc, lrc),
     write(N),
 	write('  '), display_line(Line), nl, !,
-	write('   '), last_separator(Line).
+	write('   '), outskirt_separator(Line, llc, tu, lrc).
 
 % display_line(Line).
 display_line([]).
