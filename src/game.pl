99e55363016677f381fd1efd53c947ea8b71f4c8
ManelCoutinho <manuel.monge.coutinho@gmail.com>
2019-11-13 02:37:53 +0000

    Added final comments

--- src/game.pl
+++ src/game.pl
@@ -4,36 +4,62 @@
 :- ensure_loaded('points_calculation.pl').
 :- ensure_loaded('menu.pl').
 
-
+%! order_difficulty(+FirstPlayer, +BotDifficulty, -Difficulty)
+% Orders the Difficulty tuple depending on who begins the game
+% int the human vs bot game mode
+% Case human: If the FirstPlayer is the human, then the tuple will have the BotDifficulty on the right side
 order_difficulty(human, Difficulty, _ - Difficulty).
+% Case bot: If the FirstPlayer is the bot, then the tuple will have the BotDifficulty on the left side
 order_difficulty(bot, Difficulty, Difficulty - _).
 
+%! init_game(+GameMode, -DifficultyTuple, -FirstPlayer)
+% Function that given a Game Mode returns the difficulty in a tuple (DifficultyTuple) and
+% wich is the first player (FirstPlayer) to make a move (bot or human)
+% Case pvp: Player vs PLayer -> There is no difficulty tuple and the first player is obviously a player
 init_game(pvp, _, human).
 
+% Case pvb: Player vs Bot
+% The bot's Difficulty is chosen using the bot menu (@see display_bot_menu)
+% And the first player with the help of the @see choose_order menu
+% It also calls @see order_difficulty to parse the Difficulty from the bot menu to a tuple
 init_game(pvb, DifficultyTuple, FirstPlayer) :- 
 	display_bot_menu('', Difficulty), !,
 	choose_order(FirstPlayer),
 	order_difficulty(FirstPlayer, Difficulty, DifficultyTuple).
 
+% Case bvb: Bot vs Bot
+% Both bots difficulty are chosen via the bot menu (@see display_bot_menu) and the 
+% First player is obviously a bot
 init_game(bvb, Difficulty1 - Difficulty2, bot) :- 
 	display_bot_menu('1 ', Difficulty1),!,
 	display_bot_menu('2 ', Difficulty2), !.
 
+% Case other: Returns to @see start_game.
 init_game(_, _, _):-
 	!, start_game.
 
-initial(GameMode, Difficulty, FirstPlayer) :-
-	display_menu(GameMode),
-	init_game(GameMode, Difficulty, FirstPlayer).
-
+%! play
+% Cicle that calls @see start_game and itself recursively
+% Cicle ends when player asks to exit at any point (moves input or Main Menu)
 play:-
 	start_game,
 	play.
 
+%! start_game
+% Calls @see menu_option with main argument
+% Meaning the main menu will be printed
 start_game :-
 	menu_option(main).
 
 % finish
+%! game_loop(+Board, +PlayerType, +GameMode, +DifficultyTuple, +NumberOfPasses)
+% This is the main game loop
+% The loop ends when both players had to pass their turn (one after the other): NumberOfPasses = 2
+% Case GameFinished:
+% NumberOfPasses reached its limit (2) and the Game ends
+% The final board is displayed one last time (@see display_game)
+% and the score is calculated for each player (@see points_calculation)
+% The cicle stops waiting for the Enter key to be pressed to go back to the main menu
 game_loop(Board, [Player, _], _, _, 2) :- 
 	write('\nGame Finished\n\n'),
 	display_game(Board, Player),
@@ -43,6 +69,12 @@ game_loop(Board, [Player, _], _, _, 2) :-
     write('Player 1 Points : '), write(Points1), nl, !,
 	write('Press enter to continue...'), read_string(_).
 
+% Case HasValidMoves:
+% If there is a valid move to be done (@see pass_move), the board is displayed (@see display_game)
+% and a move is chosen (@see choose_move), either by a human or a bot.
+% Then the turn is passed (@see next_player) and the game_loop is called again
+% with the number of passes reset to 0
+% A player can exit this loop by writing exit in the move input
 game_loop(Board, [Player, Type], GameMode, Difficulty, _) :-
 	\+ pass_move(Board, Player),
 	display_game(Board, Player),
@@ -50,6 +82,12 @@ game_loop(Board, [Player, Type], GameMode, Difficulty, _) :-
 	next_player([Player, Type], NextPlayer, GameMode, Difficulty, NewDifficulty),
 	game_loop(NewBoard, NextPlayer, GameMode, NewDifficulty, 0).
 
+% Case Otherwise:
+% Otherwise means that there was no valid move.
+% The board is displayed (@see display_game) and a message is shown to notify
+% the user that they lost their turn. The number of passes is incremented and 
+% called @see next_player to get the next player to make a move and its difficulty (in case of a bot)
+% Finally the game loop is called with the new values
 game_loop(Board, [Player, Type], GameMode, Difficulty, Passes) :-
 	display_game(Board, Player),
 	%pass_move(Board, Player),
--- src/interface.pl
+++ src/interface.pl
@@ -1,35 +1,50 @@
-ccA(X) :- char_code('A', X).	% A char code
-cc0(X) :- char_code('0', X).	% 0 char code
+%! ccA(?X)
+% Returns or verifies if X is the code of 'A'
+ccA(X) :- char_code('A', X).
 
-% read_move(-Move, +Board) :- returns list with the move in the format [ICN, IRN, FCN, FRN].
+%! cc0(?X)
+% Returns or verifies if X is the code of 0
+cc0(X) :- char_code('0', X).
+
+%! read_move(-Move, +Board) :- 
+% Returns list with the move in the format [ICN, IRN, FCN, FRN].
 % ICN :- Initial Column Number
 % IRN :- Initial Row Number
 % FCN :- Final Column Number
 % FRN :- Final Row Number
+% This is accomplished with the help of @see read_string to read the input
+% and a parser (@see parse_move)
 read_move(Move, Board):-
     write(' Move: '),
     read_string(Input),
     parse_move(Input, Move, Board).
 
-% parse_move(-Input, +Move, -Board) - tests the length of the input and calls @valid_move_input to validate the input
+%! parse_move(+Input, -Move, +Board)
+% Tests whether the length of the input is 4
+% and calls @see valid_move_input to validate it
 parse_move(Input, Move, Board) :-
 	length(Input, 4),
 	valid_move_input(Input, Move, Board).
 
+% Case stuck in error: calls read_move again
 parse_move(Input, Move, Board) :-
 	length(Input, 4),
 	valid_move_input(Input, _, Board), !, read_move(Move, Board).
 
-% In case the input is empty calls read_move again
+% Case Enter: If only enter was pressed the input is empty and @see read_move is called again
 parse_move([], M, B) :- !, read_move(M, B).
 
-% TODO: fazer funcao melhor
+% Case Exit: If exit is written, abort program
 parse_move(['e', 'x', 'i', 't'], _, _) :- write(' Exiting\n'), !, abort.
 
-% In case of invalid input, shows error message and fails (TODO: change to read_move again)
+% Case Invalid Input: shows error message warning the user and calls @see read_move
 parse_move(_, M, B) :- write(' Wrong format: InitialColumn InitialRow FinalColumn FinalRow (without spacing).\n Ex:a1d1\n\n'), !, read_move(M, B).
 
-% valid_move_input(-Input, +Move, -Board) :- validates each of the four input chars and transforms each one to a number
+%! valid_move_input(+Input, -Move, +Board)
+% Validates each of the four input chars 
+% by calling @see valid_column_input and @see valid_row_input
+% tests if the initial and final position are different and 
+% transforms each of the Inputs to a Number returned in the list Move in the same order
 valid_move_input([IC, IR, FC, FR], [IC_N, IR_N, FC_N, FR_N], [Line | Board]) :-
 	valid_column_input(IC, IC_N, Line),
 	valid_row_input(IR, IR_N, [Line | Board]),
@@ -37,6 +52,9 @@ valid_move_input([IC, IR, FC, FR], [IC_N, IR_N, FC_N, FR_N], [Line | Board]) :-
 	valid_row_input(FR, FR_N, [Line | Board]),
 	[IC_N, IR_N] \== [FC_N, FR_N].
 
+%! valid_column_input(+Input, -ColumnNumber, +Line)
+% Receives an atom letter as an input and parses it to a Number
+% Verifies if that ColumnNumber can be an index of the Line
 valid_column_input(Input, ColumnNumber, BoardLine) :-
 	upcase_atom(Input, Upper),	% upper cases the IC
 	char_code(Upper, CodeC),
@@ -46,39 +64,49 @@ valid_column_input(Input, ColumnNumber, BoardLine) :-
 	length(BoardLine, Comp),
 	ColumnNumber < Comp.
 
+%! valid_row_input(+Input, -RowNumber, +Line)
+% Receives a atom number as an input and
+% Verifies if that RowNumber can be an index of the Board
 valid_row_input(Input, RowNumber, Board) :-
 	atom_number(Input, RowNumber),
 	RowNumber >= 0,
 	length(Board, Comp),
 	RowNumber < Comp.
 
+%! read_string(-CharList)
+% reads the input, parses it to atom codes
+% from this to a string and finally to a list of characters that is returned
 read_string(CharList) :-
     current_input(Input),
     read_line_to_codes(Input, Codes),
     string_codes(String, Codes),
 	atom_chars(String, CharList).
 
+%! read_menu(-Option, +Type)
+% Reads Menu Input (@see read_string) and parses it depending on the type (@see parse_menu)
+% Returning a list with the character corresponding to the chosen option
 read_menu(Option, Type):-
     write(' Option: '),
     read_string(Input), 
     parse_menu(Input, Option, Type).
 
-% parse_menu([Input | []], Int, NOptions) :- 
-% 	atom_number(Input, Int),
-% 	Int =< NOptions.
-
+%! parse_menu(+Input, -Option, +Type)
+% Parses Input depending on the menu Type and
+% Returns the parsed option to the Option argument
+% May through error signal if no valid input was given
+% in this case the user will be asked for the Option again
 parse_menu(['0'], exit, main).
 parse_menu(['1'], pvp, main).
 parse_menu(['2'], pvb, main).
 parse_menu(['3'], bvb, main).
 parse_menu(['4'], instructions, main).
 
-parse_menu(['0' | []], back, bot).
-parse_menu(['1' | []], 0, bot).
-parse_menu(['2', '.', '1'  | []], 1, bot).
-parse_menu(['2', '.', '2'  | []], 2, bot).
-parse_menu(['3', '.', '1'  | []], 3, bot).
-parse_menu(['3', '.', '2'  | []], 4, bot).
+parse_menu(['0'], back, bot).
+parse_menu(['1'], 0, bot).
+parse_menu(['2', '.', '1' ], 1, bot).
+parse_menu(['2', '.', '2' ], 2, bot).
+parse_menu(['3', '.', '1' ], 3, bot).
+parse_menu(['3', '.', '2' ], 4, bot).
 
 parse_menu(['0'], back, order).
 parse_menu(['1'], human, order).
@@ -90,24 +118,33 @@ parse_menu(_, O, Type) :-
 	format(' Invalid Menu Option (choose a number from 0 to ~d)~n', [NOptions]),
 	!, read_menu(O, Type).
 
+%! noptions(?MenuType, ?NumberOfOptions)
+% Associates a MenuType with its NumberOfOptions
 noptions(main, 4).
 noptions(bot, 4).
 noptions(order, 2).
 
+%! read_dimension(-Output, +Dimension)
+% Asks for the Dimension defined (width = columns or height = rows)
+% Reads the user answer (@see read_string) and parses it
+% using @see parse_dimensions
 read_dimension(Output, Dimension):-
 	format(' Number of ~s: ', [Dimension]),
     read_string(Input),
     parse_dimension(Input, Output, Dimension).
 
+%! parse_dimension(+Input, -Output, +Dimension)
+% Parses the Input verifying if it is an integer
+% between 0 (minimum board size) and 8 (maximum board size)
 parse_dimension([Input], Output, _) :-
 	atom_number(Input, Output),
 	Output > 0, Output < 8, !.
 
-% In case the input is empty calls read_move again
+% Case Enter: If only enter was pressed the input is empty and @see read_dimension is called again
 parse_dimension([], Output, Dimension) :- !, read_dimension(Output, Dimension).
 
-% TODO: fazer funcao melhor
+% Case Exit: If exit is written, abort program immediatly
 parse_dimension(['e', 'x', 'i', 't'], _, _) :- write(' Exiting\n'), !, abort.
 
-% In case of invalid input, shows error message and fails (TODO: change to read_move again)
+% Case Invalid Input: shows error message warning the user about the minimum and maximum board sizes and calls @see read_dimension
 parse_dimension(_, Output, Dimension) :- format(' ~s must be an integer between 0 and 8~n', [Dimension]), !, read_dimension(Output, Dimension).
