33c43891525650f9b37a3e616087eac3fc7794d1
ManelCoutinho <manuel.monge.coutinho@gmail.com>
2019-11-13 01:43:50 +0000

    Comment Display

--- src/board_pieces.pl
+++ src/board_pieces.pl
@@ -1,22 +1,21 @@
-clear :- write('\e[2J\n').
+clear :- write('\e[2J\n'). % Clears entire screen
 
 ulc :- ansi_format([fg(cyan)], '~c', [9556]). % ╔ : upper left corner
 urc :- ansi_format([fg(cyan)], '~c', [9559]). % ╗ : upper right corner
 llc :- ansi_format([fg(cyan)], '~c', [9562]). % ╚ : lower left corner
 lrc :- ansi_format([fg(cyan)], '~c', [9565]). % ╝ : lower right corner
 
-hdiv :- halfhdiv, halfhdiv. % ═ : horizontal division
+hdiv :- halfhdiv, halfhdiv. % ══ : horizontal division
 vdiv :- ansi_format([fg(cyan)], '~c', [9553]). % ║ : vertical division
 mdiv :- ansi_format([fg(cyan)], '~c', [9580]). % ╬ : middle division
 
-halfhdiv :- ansi_format([bold, fg(cyan)], '~c', [9552]).
+halfhdiv :- ansi_format([bold, fg(cyan)], '~c', [9552]). % ═: horizontal division (half because length of hdiv is 2)
 
 tr :- ansi_format([fg(cyan)], '~c', [9568]). % ╠ : T right
 tl :- ansi_format([fg(cyan)], '~c', [9571]). % ╣ : T left
 td :- ansi_format([fg(cyan)], '~c', [9574]). % ╦ : T down
 tu :- ansi_format([fg(cyan)], '~c', [9577]). % ╩ : T up
 
-% TODO: mudar cores
 tr_oneline :- ansi_format([fg(cyan)], '~c', [9567]). % ╟ : T right one line
 tl_oneline :- ansi_format([fg(cyan)], '~c', [9570]). % ╢ : T left one line
 
@@ -24,7 +23,7 @@ tl_oneline :- ansi_format([fg(cyan)], '~c', [9570]). % ╢ : T left one line
 wt:- ansi_format([bold, fg(white)], '~c', [11044]), write(' '). % White piece
 bl:- ansi_format([bold, fg(black)], '~c', [11044]), write(' '). % Black piece
 
-logo_sq :- ansi_format([bold, fg(black)], '~s', ['█']).
+logo_sq :- ansi_format([bold, fg(black)], '~s', ['█']). % Black Bold square for logo printing
 
 null:- write('  '). % Null space
 corner:- write(' '). % Board corner
--- src/display.pl
+++ src/display.pl
@@ -3,26 +3,70 @@
 :- ensure_loaded('board_generation.pl').
 
 % created to ensure there's a clear separation line between board lines
+%! middle_separator(+Line, +Head, +End)
+% Prints separators for the middle Line's
+% using the help of @see middle_separator_helper
+% Begins by printing the Head of the Lin and then calls the helper
 middle_separator(Line, Head, Tail) :- 
 	write(' '), Head, 
 	middle_separator_helper(Line, Tail).
 
 % create a separator between the board and the bottom left and right corners
+
+%! middle_separator_helper(+Line, +End)
+% Helps to print separators for the middle Line's
+% Switches between hdiv and midv until the Line is empty, when it prints the End
 middle_separator_helper([_ | []], Element) :- hdiv, Element, nl.
 
 middle_separator_helper([_ | T], Last) :- hdiv, mdiv, middle_separator_helper(T, Last).
 
-% creates the outskirt lines for board
+%! outskirt_separator_helper(+Line, +Middle, +End)
+% Helps to print separators for the outskirt Line's
+% Switches between hdiv and Middle until the Line is empty, when it prints the End
 outskirt_separator_helper([_ | []], _, End) :- hdiv, End, nl.
 
 outskirt_separator_helper([_ | R], Middle, End) :- hdiv, Middle, outskirt_separator_helper(R, Middle, End).
 
+%! outskirt_separator(+Line, +Head, +Middle, +End)
+% Prints separators for the outskirt Line's
+% using the help of @see outskirt_separator_helper
+% Begins by shortening the Line by 2 and prints the Head of the Line
+% (helper takes care of the rest)
 outskirt_separator([_, _ | Rest], Head, Middle, End) :-
 	write(' '), Head,
 	outskirt_separator_helper(Rest, Middle, End).
 
-% general_line_display(Line, Row_Number). -> 2 - 6
-general_line_display([],_).
+%! display_first_line(+Board, +RowIndex). -> 0
+% Displays firs line of the Board and prints its index (RowIndex)
+% Calls @see outskirt_separator to display the first separator
+% with ulc as the head of the line, td as the middle separators and urc as the end
+% Uses @see display_line to display the line and
+% calls @see display_second_line to print the second line after incrementing the row index
+display_first_line([H | T], N) :-
+	write('   '), outskirt_separator(H, ulc, td, urc),
+	write(N), write('  '), display_line(H), nl, !,
+	N1 is N + 1,
+	display_second_line(T, N1).
+
+% display_second_line(+Board, +RowIndex). -> 1
+% Displays second line of the Board and prints its index (RowIndex)
+% Calls @see middle_separator to display the second separator
+% with ulc as the head of the line and urc as the end
+% Uses @see display_line to display the line and, after incrementing the row index,
+% calls @see general_line_display to print every other line except the last one
+display_second_line([H | T], N) :-
+	middle_separator(H, ulc, urc),
+	write(N), vdiv, display_line(H), nl, !,
+	N1 is N + 1,
+	general_line_display(T, N1).
+
+% general_line_display(+Board, +RowIndex). -> [2:N[
+% Displays every other line of the Board and prints its index (RowIndex)
+% Calls @see middle_separator to display the middle separator
+% with tr as the head of the line and tl as the end
+% Uses @see display_line to display the line and, after incrementing the row index,
+% calls itself to print every other line except the last one, when it
+% calls @see display_last_line.
 general_line_display([H | []], N):-
 	display_last_line(H, N), !.
 general_line_display([H | T], N) :-
@@ -31,28 +75,22 @@ general_line_display([H | T], N) :-
 	N1 is N + 1,
 	general_line_display(T, N1).
 
-% display_second_line(Line, Row_Number). -> 1
-display_second_line([H | T], N) :-
-	middle_separator(H, ulc, urc),
-	write(N), vdiv, display_line(H), nl, !,
-	N1 is N + 1,
-	general_line_display(T, N1).
-
-% display_first_line(Board, Row_Number). -> 0
-display_first_line([H | T], N) :-
-	write('   '), outskirt_separator(H, ulc, td, urc),
-	write(N), write('  '), display_line(H), nl, !,
-	N1 is N + 1,
-	display_second_line(T, N1).
-
-% display_last_line(Line, Row_Number). -> 7
+% display_last_line(+Line, +RowIndex). -> N
+% Displays last Line of the Board and prints its index (RowIndex)
+% Calls @see middle_separator to display the second last separator
+% with llc as the head of the line and lrc as the end
+% Uses @see display_line to display the line and, finally,
+% calls @see outskirt_separator to display the last separator
+% with çlc as the head of the line, tu as the middle separators and lrc as the end
 display_last_line(Line, N) :-
 	middle_separator(Line, llc, lrc),
     write(N),
 	write('  '), display_line(Line), nl, !,
 	write('   '), outskirt_separator(Line, llc, tu, lrc).
 
-% display_line(Line).
+%! display_line(+Line).
+% Displays Line (middle part)
+% Ex: ║⬤║  ║  ║  ║  ║⬤║
 display_line([]).
 display_line([corner | []]):-
 	corner,
@@ -61,11 +99,19 @@ display_line([Element | Rest]) :-
 	Element, vdiv,
 	display_line(Rest).
 
+%! display_game(+Board, +Player)
+% Prints player turn and
+% Uses @see write_columns_index to print the columns indexes defining the first index to A
+% and @see display_first_line that will print the board by calling other functions
+% When calling display_first_line it also defines the first row index to 0
 display_game([Line | TBoard], Player):-
-	write('Player turn: '), write(Player), nl, nl,
+	format(' Player turn: ~d~n~n', [Player]),
 	write_columns_index(Line, 'A'),
 	display_first_line([Line | TBoard], 0).
 
+%! write_columns_helper(+Line, +Letter)
+% Helper function that writes current Letter and "increments" it in alphabetical order
+% until Line is empty, when it ends with a new line.
 write_columns_helper([_ | []], Letter) :-
 	!, write(Letter), nl.
 
@@ -76,5 +122,8 @@ write_columns_helper([_ | TLine], Letter) :-
 	char_code(NextLetter, NextLetterCode),
 	write_columns_helper(TLine, NextLetter).
 
+%! write_columns_index(+Line, +Letter)
+% Uses @see write_columns_helper to write column indexes starting with letter Letter
+% Line is used to count number of columns
 write_columns_index(Line, Letter) :-
 	write('   '), write_columns_helper(Line, Letter).
