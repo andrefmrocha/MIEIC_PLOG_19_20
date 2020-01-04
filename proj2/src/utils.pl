adb0216e18523ffdc01c2a32e086005bd5cfccdd
Manuel Coutinho <manuel.monge.coutinho@gmail.com>
2020-01-04 22:24:03 +0000

    Code cleanup

--- proj2/src/utils.pl
+++ proj2/src/utils.pl
@@ -22,3 +22,14 @@ flatten([], []).
 flatten([H | T], FlattenB) :-
 	flatten(T, NewFlatten),
 	append(H, NewFlatten, FlattenB).
+
+%! Utility function that maps 0s to empty variable
+map_sol([], []).
+map_sol([1 | T1], [X | T2]):-
+    X#=1,
+    map_sol(T1, T2).
+map_sol([2 | T1], [X | T2]):-
+    X#=2,
+    map_sol(T1, T2).
+map_sol([0 | T1], [_ | T2]):-
+    map_sol(T1, T2).
